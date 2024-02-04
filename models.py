fdff22ba2e994aafba819f4d79d9369af6d43513
bagher <palahangmohammadbagher@gmail.com>
2024-02-04 10:51:29 +0330

    bagher

---
+++ models.py
@@ -0,0 +1,123 @@
+import mysql.connector
+from datetime import datetime
+import utils
+
+
+class User:
+    def __init__(self, host:str, port:int, user:str, password:str, database:str):
+        
+        self.isAuthenticated = False
+        self.user = None
+
+        self.connection = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database,
+        )
+    
+        self.cursor = self.connection.cursor()
+        self.create_table()
+
+    def create_table(self):
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            mobile_number VARCHAR(11),
+            birthdate DATE NOT NULL,
+            register_date DATE NOT NULL,
+            last_login_date DATE NOT NULL,
+            last_login_time TIME NOT NULL
+        )
+        """
+
+        self.cursor.execute(create_table_query)
+        self.connection.commit()
+
+    def register_user(self, username:str, password:str, email:str, birthdate:str, mobile_number:str=None):
+
+        self.cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+        existing_username = self.cursor.fetchone()
+
+        if existing_username:
+            print(f"Error: Username '{username}' is already taken. Please choose a different username.")
+            return
+
+        self.cursor.execute("SELECT id FROM users WHERE email = %s", (email,))
+        existing_email = self.cursor.fetchone()
+
+        if existing_email:
+            print(f"Error: Email '{email}' is already used. Please choose a different email.")
+            return
+        
+        if not utils.validating_email(email):
+            print(f"Error: Email '{email}' is not a valid email. Please use a different email.")
+            return
+
+        if not utils.validating_username(username):
+            print("Error: Username should contain both uppercase and lowercase word . Please use a different username.")
+            return
+    
+        if not utils.validating_password(password):
+            print("Error: Password should be mpre than 8 character and contain uppercase, lowercase, number and spercial signs . Please use a different username.")
+            return
+
+        if not utils.validating_mobile_number(mobile_number):
+            print(f"Error: Mobile Mumber '{mobile_number}' is not a valid email. Please use a different email.")
+            return
+
+
+
+        insert_query = """
+            INSERT INTO users (username, password, email, mobile_number, birthdate, register_date, last_login_date, last_login_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+
+#       TODO : add last login time
+        user_data = (
+            username,
+            password,
+            email,
+            mobile_number,
+            birthdate,
+            datetime.now().date(),
+            datetime.now().date(),
+            '10:23'
+        )
+
+        self.cursor.execute(insert_query, user_data)
+        self.connection.commit()
+
+        self.login_user(username, password)
+
+    def login_user(self, username, password):
+        if self.isAuthenticated:
+            print('You already logged in please logout first')
+            return
+        
+        self.cursor.execute("SELECT id FROM users WHERE username = %s AND password = %s", (username, password))
+        user = self.cursor.fetchone()
+
+        if not user:
+            print('Authentication failed. password or email may be wrong')
+            return
+        
+        # TODO : change last login and save user object in class atr
+        
+        self.isAuthenticated = True
+        self.user = user[0]
+        print(f'Dear {self.user} you have just logged in')
+        return
+
+
+    def logout(self):
+        self.isAuthenticated = False
+        self.user = None
+
+
+user = User('tai.liara.cloud', 33428, 'root', 'ErOmibw13imQzlzw3TIgyE10', 'focused_driscoll')
+# print(user.isAuthenticated)
---
+++ utils.py
@@ -0,0 +1,14 @@
+def validating_email(email):
+    return True
+
+def validating_username(username):
+    return True
+
+def validating_password(password):
+    return True
+
+def validating_mobile_number(mobile_number):
+    return True
+
+def haslib_password(password):
+    return password

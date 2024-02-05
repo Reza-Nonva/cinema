7783507f847caa5a20209d60ac53a6430ae6bf2a
bagher <palahangmohammadbagher@gmail.com>
2024-02-05 13:24:12 +0330

    change password and profile

--- models.py
+++ models.py
@@ -3,22 +3,23 @@ from db import DB_obj
 import utils
 
 class User:
-    def __init__(self):
-        
+    def __init__(self, connection, cursor):
+        self.connection = connection
+        self.cursor = cursor
         self.isAuthenticated = False
         self.user = None
 
-    def register_user(self, connection, cursor, username:str, password:str, email:str, birthdate:str, mobile_number:str=None):
+    def register_user(self, username:str, password:str, email:str, birthdate:str, mobile_number:str=None):
 
-        cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
-        existing_username = cursor.fetchone()
+        self.cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+        existing_username = self.cursor.fetchone()
 
         if existing_username:
             print(f"Error: Username '{username}' is already taken. Please choose a different username.")
             return
 
-        cursor.execute("SELECT id FROM users WHERE email = %s", (email,))
-        existing_email = cursor.fetchone()
+        self.cursor.execute("SELECT id FROM users WHERE email = %s", (email,))
+        existing_email = self.cursor.fetchone()
 
         if existing_email:
             print(f"Error: Email '{email}' is already used. Please choose a different email.")
@@ -55,48 +56,116 @@ class User:
             datetime.now().time(),
         )
 
-        cursor.execute(insert_query, user_data)
-        connection.commit()
+        self.cursor.execute(insert_query, user_data)
+        self.connection.commit()
 
-        self.login_user(connection, cursor, username, password)
+        self.login_user(username, password)
 
-    def login_user(self, connection, cursor, username, password):
+    def login_user(self, username, password):
         if self.isAuthenticated:
             print('You already logged in please logout first')
             return
         
-        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, utils.hash_password(password)))
-        user_obj = cursor.fetchone()
+        self.cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, utils.hash_password(password)))
+        user_obj = self.cursor.fetchone()
 
         if not user_obj:
             print('Authentication failed. password or email may be wrong')
             return
                 
-        columns = [column[0] for column in cursor.description]
+        columns = [column[0] for column in self.cursor.description]
         self.user = dict(zip(columns, user_obj))
 
         self.isAuthenticated = True
 
-        cursor.execute("UPDATE users SET last_login_time = %s WHERE id = %s", (datetime.now().time() ,self.user['id']))
-        connection.commit()
+        self.cursor.execute("UPDATE users SET last_login_time = %s WHERE id = %s", (datetime.now().time() ,self.user['id']))
+        self.connection.commit()
         print(f"Dear {self.user['username']} you have just logged in")
         return
 
-    def change_profile(self):
-        pass
+    def change_profile(self, username=None, email=None, mobile_number=None):
+        if not self.isAuthenticated:
+            print("Error: User should be logged in first.")
+            return
+        
+        if username:
+            self.cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+            existing_username = self.cursor.fetchone()
 
-    def change_password(self):
-        pass
+            if existing_username:
+                print(f"Error: Username '{username}' is already taken. Please choose a different username.")
+            else:
+                if utils.validating_username(username):
+                    self.cursor.execute("UPDATE users SET username = %s WHERE id = %s", (username, self.user['id']))
+                    print(f"Dear {self.user['username']} you have just change your username")
+                else:
+                    print("Error: Username should contain both uppercase and lowercase word . Please use a different username.")
+
+        if email:
+            self.cursor.execute("SELECT id FROM users WHERE email = %s", (email,))
+            existing_email = self.cursor.fetchone()
+
+            if existing_email:
+                print(f"Error: Email '{email}' is already taken. Please choose a different email.")
+            else:
+                if utils.validating_email(email):
+                    self.cursor.execute("UPDATE users SET email = %s WHERE id = %s", (email, self.user['id']))
+                    print(f"Dear {self.user['username']} you have just change your email")
+                else:
+                    print(f"Error: Email '{email}' is not a valid email. Please use a different email.")
+
+        if mobile_number:
+            if utils.validating_mobile_number(mobile_number):
+                self.cursor.execute("UPDATE users SET email = %s WHERE id = %s", (email, self.user['id']))
+                print(f"Dear {self.user['username']} you have just change your mobile number")
+            else:
+                print(f"Error: Mobile Mumber '{mobile_number}' is not a valid email. Please use a different mobile number.")
+
+        self.connection.commit()
+
+
+    def change_password(self, old_password, new_password, new_password_confirm):
+        if not self.isAuthenticated:
+            print("Error: User should be logged in first.")
+            return
+
+        if new_password != new_password_confirm:
+            print("Error: new_password and new_password_confirm should be the same.")
+            return
+        
+        if not utils.validating_password(new_password):
+            print("Error: Password should be mpre than 8 character and contain uppercase, lowercase, number and spercial signs . Please use a different password.")
+            return
+
+        self.cursor.execute("SELECT id FROM users WHERE username = %s AND password = %s", (self.user['username'], utils.hash_password(old_password)))
+        user_id = self.cursor.fetchone()
+
+        if not user_id:
+            print('Error: username or old password is incorrect. please check and fill again.')
+            return
+        
+        self.cursor.execute("UPDATE users SET password = %s WHERE id = %s", (utils.hash_password(new_password), self.user['id']))
+        self.connection.commit()
+
+        print(f"Dear {self.user['username']} you have just change your password")
+        return
 
     def logout(self):
         self.isAuthenticated = False
         self.user = None
 
+    def __str__(self):
+        if not user.isAuthenticated:
+            print('You are not logged in')
+            return
+        print(self.user['username'])
 
-user = User()
-
-user.register_user(DB_obj.connection, DB_obj.cursor, 'Bagher5', 'Thisis@p@ssword1', 'palahangmohammadbagher5@gmail.com', '1382-06-01', '0902341014')
-# user.login_user(DB_obj.connection, DB_obj.cursor, 'ali', 'fuckyou2')
+user = User(DB_obj.connection, DB_obj.cursor)
+# user.register_user('Bagher6', 'Thisis@p@ssword1', 'palahangmohammadbagher6@gmail.com', '1382-06-01', '09023241014')
+user.login_user('ali', 'Thisis@p@ssword1')
+# user.change_password('Thisis@p@ssword1', 'Thisis@p@ssword2', 'Thisis@p@ssword2')
+user.login_user('Alireza1', 'Thisis@p@ssword2')
+user.change_profile(username='Alireza2', email='fuck@thefucking.world')
 
 class Movie:
     name:str
